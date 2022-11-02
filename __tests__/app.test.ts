1c53aa416e74842376bc68c44c463981d08489ed
Paattis <leevi.paattilammi@gmail.com>
2022-11-02 18:50:37 +0200

    run prettier

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,12 +1,10 @@
-import request from 'supertest';
+import request from "supertest";
 
-import app from '../src/app';
+import app from "../src/app";
 
-describe('GET /', () => {
+describe("GET /", () => {
   test('hello world route should return "Hello world!" /', async () => {
-
-    const res = await request(app).get('/').expect(200)
-    expect(res.body).toEqual({msg: 'Hello world!'})
-
+    const res = await request(app).get("/").expect(200);
+    expect(res.body).toEqual({ msg: "Hello world!" });
   });
 });
