9fba27d5ffbdfb436cdc5bd042355df5b2dcc35c
Niklas Seppala <niklas.t.seppala@gmail.com>
2022-11-06 11:00:00 +0200

    Add same .prettierrc as in frontend, add npm scripts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-//import express = require('express');
-import app from "./app";
+import app from './app';
 const port = process.env.PORT || 3000;
 
 app.listen(port, (): void =>
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,9 +1,9 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response): void => {
-  res.send({ msg: "Hello world!" });
+router.get('/', (req: Request, res: Response): void => {
+  res.send({ msg: 'Hello world!' });
 });
 
 export { router };
