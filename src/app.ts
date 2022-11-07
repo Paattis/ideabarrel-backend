35836c1fe2c8faad1b2cf72f756e78edcd94a6ed
Niklas Seppälä <66251857+Niklas-Seppala@users.noreply.github.com>
2022-11-07 20:07:19 +0200

    Add logger (#5)

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application } from 'express';
 import { router as indexRoutes } from './routes/index.routes';
+import {
+  httpBegin as httpLogger,
+  httpEnd as httpEndLogger,
+  httpError as httpErrorLogger,
+} from './logger/log';
 
 const app: Application = express();
 
+// Loggers for http requests.
+app.use(httpLogger);
+app.use(httpEndLogger);
+
+// Now set the routrs
 app.use('/', indexRoutes);
+
+// Finally error logger
+app.use(httpErrorLogger);
+
 export default app;
---
+++ src/logger/transports.ts
@@ -0,0 +1,20 @@
+import { transports, format } from 'winston';
+
+const ftime = 'YYYY-MM-DD HH:mm:ss:ms';
+
+const base = format.combine(
+  format.timestamp({ format: ftime }),
+  format.printf(
+    ({ timestamp, level, message }) => `${timestamp} [${level}] ${message}`
+  )
+);
+
+export const devConsole = new transports.Console({
+  format: format.combine(format.colorize(), base),
+});
+
+export const file = new transports.File({
+  format: base,
+  dirname: 'logs',
+  filename: 'server.log',
+});
