3a11e74b92d9c2e5d2407ab0978314c2a07ca5bf
Niklas Seppala <niklas.t.seppala@gmail.com>
2022-12-04 21:33:11 +0200

    asd

--- src/db/Accessor.ts
+++ src/db/Accessor.ts
@@ -2,6 +2,10 @@ import { CommentsClient } from './CommentClient';
 import { AppPrismaContext } from './context';
 import { IdeasClient } from './IdeasClient';
 import { LikesClient } from './LikesClient';
+
+
+
+
 import { RolesClient } from './RolesClient';
 import { TagsClient } from './TagsClient';
 import { UserClient } from './UserClient';
