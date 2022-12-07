7e878ba4a1663dde0103b12935f73e1913cf6624
Paattis <leevi.paattilammi@gmail.com>
2022-12-07 20:40:14 +0200

    run prettier again, fix tests

--- src/utils/httpsRedirect.ts
+++ src/utils/httpsRedirect.ts
@@ -1,7 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 
 export const httpsRedirect = async (req: Request, res: Response, next: NextFunction) => {
-  if (process.env.APP_ENV !== 'DEVELOPEMENT' && !req.secure) {
+  if (
+    process.env.APP_ENV != undefined &&
+    process.env.APP_ENV === 'PRODUCTION' &&
+    !req.secure
+  ) {
     const newUrl = `https://${process.env.SERVER_IP}${req.url}`;
     return res.redirect(newUrl);
   }
